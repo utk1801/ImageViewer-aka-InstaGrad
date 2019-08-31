5840eb6f7ac38e952040d07b8311e88f8f71fc68
Ashish2211 <42649141+Ashish2211@users.noreply.github.com>
2019-08-31 19:28:44 +0530

    login page updated

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -7,6 +7,8 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import { FormHelperText } from '@material-ui/core';
+import Home from '../home/Home';
+import ReactDOM from 'react-dom';
 
 class Login extends Component {
 
@@ -17,13 +19,34 @@ class Login extends Component {
             usernameRequired: "dispNone",
             password: "",
             passwordRequired: "dispNone",
-            accessToken: ""
+            accessToken: "",
+            loggedIn: false,
+            loggedInRequired:"dispBlock"
+
         };
     }
 
     loginClickHandler = () => {
-        this.state.username === "aa" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: 'dispNone' })
-        this.state.password === "123" ? this.setState({ passwordRequired: 'dispBlock' }) : this.setState({ passwordRequired: 'dispNone' })
+        let username = "aa";
+        let password = "123";
+        let accessToken = "18489759624.aee6821.0878e86f66df43fa99202ff08b765816";
+        let xhrLogin = new XMLHttpRequest();
+        let that = this;
+        this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: 'dispNone' })
+        this.state.password === "" ? this.setState({ passwordRequired: 'dispBlock' }) : this.setState({ passwordRequired: 'dispNone' }) 
+        if(this.state.username === username && this.state.password === password && this.state.loggedIn === false){
+            ReactDOM.render(<Home />, document.getElementById('root'));
+            sessionStorage.setItem('accessToken', xhrLogin.getResponseHeader(accessToken));
+            that.setState({
+                loggedIn: true
+            });
+        }
+
+        else if(this.state.loggedIn === false){
+            
+            this.state.loggedIn = true;
+        }
+
     }
 
     inputUsernameChangeHandler = (e) => {
@@ -40,6 +63,7 @@ class Login extends Component {
                 <header className="app-header" >
                     <div className="logo"> Image Viewer</div>
                 </header>
+                
                 <div className="login-card">
                     <Card>
                         <div className="login-header">LOGIN</div>
@@ -55,7 +79,19 @@ class Login extends Component {
                                 <Input id="password" type="password" password={this.state.password}
                                     onChange={this.inputPasswordChangeHandler} />
                                 <FormHelperText className={this.state.passwordRequired}><span className="red">required</span></FormHelperText>
-                            </FormControl>
+                            </FormControl><br/><br/>
+                            
+
+                            {this.state.loggedIn === true &&
+                                <FormControl>
+                                <FormHelperText className = {this.state.loggedInRequired}>
+                                    <span className="red">
+                                        Incorrect username and/or password
+                                    </span>
+                                    </FormHelperText>
+                                </FormControl>
+                            }
+
                             <Button className="login-button" variant="contained" color="primary"
                                 onClick={this.loginClickHandler}>LOGIN</Button>
                         </CardContent>
